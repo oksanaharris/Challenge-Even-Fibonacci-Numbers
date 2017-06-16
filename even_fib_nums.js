9dcf19d617aed74bdb35d70995204cb60f01ad49
Oksana Harr <oksana.harris@gmail.com>
2017-06-16 10:06:08 -1000

    finished

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 /**
  * Return the total sum of all even fibonacci numbers up to and including
  * the value given to you at `maxFibValue`
@@ -5,31 +7,90 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
+
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  // var fibArr = [0,1];
+  // var evenFibArr = [];
 
-  // do your work here
+  let num1 = 0;
+  let num2 = 1;
+  let num3 = 0;
+  
+  // console.log(num1, num2, num3);
 
+  while (num3 <= maxFibValue) {
+    num3 = num1 + num2;
+    // console.log(num3);
+    if (num3 <=maxFibValue && num3 % 2 === 0){
+      // evenFibArr.push(num3);
+      sum += num3;
+    }
+    // fibArr.push(num3);
+    num1 = num2;
+    num2 = num3;
+  }
+
+  // console.log(fibArr);
+  // console.log(evenFibArr);
+  // // sum = evenFibArr.reduce( (prev, curr) => prev + curr );
+  // console.log(sum);
   return sum;
 }
 
+console.log(_sumFibs(100));
+
 // bonus round
+
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
+  // var fibArr = [0,1];
 
-  //define your base case, validate your input
+  let num1 = 0;
+  let num2 = 1;
+  let num3 = 0;
+  
+  // console.log(num1, num2, num3);
 
-
-  //do your work here
+  while (num3 < maxFibValue) {
+    num3 = num1 + num2;
+    // console.log(num3);
+    if (num3 < maxFibValue){
+      highest = num3;
+    }
+    // fibArr.push(num3);
+    num1 = num2;
+    num2 = num3;
+  }
+  // console.log(fibArr);
+  // console.log(highest);
 
   return highest;
-};
+}
+
+function _findNth (n){
+  var fibArr = [0,1];
+
+  var atIndexN;
+
+  for (let i = 2; i <= n; i++){
+    fibArr[i] = fibArr[i-1] + fibArr[i-2];
+  }
+
+  // console.log (fibArr);
+
+  atIndexN = fibArr[n];
+
+  return atIndexN;
+}
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
+
 module.exports = {
   sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  highestFibonacciNumber : _highestFibonacciNumber,
+  findNth : _findNth
 };
--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -16,11 +16,12 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
 
   // when the 2 tests pass, unskip the next test and
   // change the expected value to the correct result
-  it.skip( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
+
+  it( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
     var sum = fibonnaciNums.sumFibs(4000000);
 
     // CHANGE THE VALUE OF 'X' to the correct number
-    sum.should.equal( 'X' );
+    sum.should.equal( 4613732 );
   });
 });
 
@@ -33,7 +34,7 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
  */
 
 // unskip the following test to test your highestFibonacciNumber() function
-describe.skip( 'Highest number in Fibonacci sequence', function() {
+describe( 'Highest number in Fibonacci sequence', function() {
   it( 'should return the highest Fibonacci number less than the given input', function() {
     var highest = fibonnaciNums.highestFibonacciNumber( 100 );
     highest.should.equal( 89 );
@@ -42,3 +43,10 @@ describe.skip( 'Highest number in Fibonacci sequence', function() {
 
 
 //STRETCH GOALS: Define another way you could write a function for a fibonacci number and write passing tests
+
+describe( 'Number in Fibonacci sequence at Nth index position in the sequence', function() {
+  it( 'should return the Fibonacci number in the Nth index position of the sequence', function() {
+    var atIndexN = fibonnaciNums.findNth( 22 );
+    atIndexN.should.equal( 17711 );
+  });
+});
