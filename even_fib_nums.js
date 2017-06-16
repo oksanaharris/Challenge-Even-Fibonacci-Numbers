ab2f6b8af5d0a2320433d940e39caa4533a910d0
Oksana Harr <oksana.harris@gmail.com>
2017-06-16 11:59:36 -1000

    working, but needs separating into cash register and calculator

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -16,8 +16,6 @@ function _sumFibs( maxFibValue ) {
   let num1 = 0;
   let num2 = 1;
   let num3 = 0;
-  
-  // console.log(num1, num2, num3);
 
   while (num3 <= maxFibValue) {
     num3 = num1 + num2;
